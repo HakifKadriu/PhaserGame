00012fac434c603f715f90eceae02d29c430bdbb
Kifaa <hakifkadriu2003@gmail.com>
2024-08-02 20:51:45 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    // Load assets
+}
+
+function create() {
+    // Create game elements
+}
+
+function update() {
+    // Update game logic
+}
